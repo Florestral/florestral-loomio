3aaa9629a7376988d931f8dd1b350c9bacec81be
Dan Barrett <dan@dover.com>
2025-01-28 08:49:49 -0600

    adds ransack to lot admin

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,3 +1,13 @@
 class Lot < ApplicationRecord
   has_and_belongs_to_many :users
+
+  # Define searchable attributes for Ransack
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "title", "location", "size", "description", "created_at", "updated_at"]
+  end
+
+  # Optionally define which associations are searchable (if any)
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
