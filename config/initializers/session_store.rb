1913e182b6ecf6711ac52008b6d1419c359af6ab
Dan Barrett <dan@dover.com>
2025-01-30 09:38:30 -0600

    trying to make mobile safari work

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,4 @@
-Loomio::Application.config.session_store :cookie_store, key: '_loomio', secure: (ENV['FORCE_SSL'].to_i == 1)
+Loomio::Application.config.session_store :cookie_store,
+  key: '_loomio',
+  secure: (ENV['FORCE_SSL'].to_i == 1),
+  same_site: :none
