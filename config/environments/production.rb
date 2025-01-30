99ce7bbe63b0506a46c8a1652808cf85dcf6b660
Dan Barrett <dan@dover.com>
2025-01-30 15:34:08 -0600

    test removing CSRF

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
+  #
+  config.action_controller.allow_forgery_protection = false
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
